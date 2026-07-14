551115c9111a774940030521bb5c87d7ebb417be
unknown <GHOSTxxx123>
2026-07-14 23:35:09 +0500

    qwe

--- back/db.py
+++ back/db.py
@@ -4,7 +4,6 @@ import random, string
 import shutil
 
 
-
 class db():
     def __init__(self):
         if not path.exists("brands"):
@@ -19,7 +18,7 @@ class db():
 
     def addBrand(self, brand):
         try:
-            if path.exists("brands/" + brand):
+            if path.exists(f"brands/{brand}"):
                 return (False, "Бренд уже существует")
             mkdir("brands/" + brand)
             return (True, "Успешно создан")
@@ -27,22 +26,81 @@ class db():
             return (False, "Ошибка: " + str(e))
 
 
-    def addModel(self, brand, model):
+    def removeBrand(self, brand):
         try:
-            if path.exists("brands/" + brand + model + ".xlsx"):
-                return (False, "Модель уже существует")
-            wb = Workbook()
-            ws = wb.active
-            ws.title = "Prices"
-            ws.cell(row=1, column=1).value = "Качество"
-            ws.cell(row=1, column=2).value = "Цена"
-            ws.cell(row=1, column=3).value = "Количество"
-            ws.cell(row=1, column=4).value = "ID"
-            wb.save(f"brands/{brand}/" + model + ".xlsx")
-            return (True, "Модель успешно созданна.")
+            namePath = f"brands/{brand}"
+            if not path.exists(namePath):
+                return (False, "Модели не существует.")
+            shutil.rmtree(namePath)
+            return (True, "Успешно удалён бренд: " + brand)
         except Exception as e:
             return (False, "Ошибка: " + str(e))
         
+    
+    def getBrands(self):
+        try:
+            if not path.exists("brands"):
+                return (False, "Брендов не существует.")
+            brands = []
+            for item in listdir("brands"):
+                if path.isdir(path.join("brands", item)):
+                    brands.append(item)
+            return (True, brands)
+        except Exception as e:
+            return (False, "Ошибка: " + str(e))
+
+
+    def addModel(self, brand, model):
+        try:
+            if path.exists(f"brands/{brand}/{model}"):
+                return (False, "Модель уже существует")
+            wb = Workbook()
+            wb.save(f"brands/{brand}/{model}.xlsx")
+            return (True, "Модель успешно созданна.")
+        except Exception as e:
+            return (False, "Ошибка: " + str(e))
+
+
+    def getModels(self, brand):
+        try:
+            pathName = f"brands/{brand}"
+            if not path.exists(pathName):
+                return (False, "Бренд не найдены")
+            
+            models = []
+            for item in listdir(pathName):
+                full_path = path.join(pathName, item)
+                if path.isfile(full_path):
+                    models.append(item[:-5])
+            return (True, models)
+        except Exception as e:
+            return (False, "Ошибка: " + str(e))
+    
+
+    def removeModel(self, brand, model):
+        try:
+            namePath = f"brands/{brand}/{model}.xlsx"
+            if not path.exists(namePath):
+                return (False, "Модели не существует.")
+            remove(namePath)
+            return (True, "Успешно удаленна модель: " + model)
+        except Exception as e:
+            return (False, "Ошибка: " + str(e))
+
+
+    def addSparePart(self, brand, model, sparePart):
+        try:
+            if not path.exists(f"brands/{brand}/{model}.xlsx"):
+                return (False, "Данной модели не существует")
+            wb = load_workbook(f"brands/{brand}/{model}.xlsx")
+            if sparePart in wb.sheetnames:
+                return (False, "Данная запчасть уже существует")
+            wb.create_sheet(sparePart)
+            wb.save(f"brands/{brand}/{model}.xlsx")
+            return (True, "Данная запчасть успешно созданна")
+        except Exception as e:
+            return (False, "Ошибка: " + str(e))
+    
     
     def addDisplay(self, brand, model, display, price, amount):
         try:
@@ -64,37 +122,7 @@ class db():
             return (True, "Дисплей успешно добавлен.")
         except Exception as e:
             return (False, "Ошибка: " + str(e))
-
-    
-    def getBrands(self):
-        try:
-            if not path.exists("brands"):
-                return (False, "Брендов не существует.")
-            brands = []
-            for item in listdir("brands"):
-                if path.isdir(path.join("brands", item)):
-                    brands.append(item)
-            return (True, brands)
-        except Exception as e:
-            return (False, "Ошибка: " + str(e))
-
-        
-    
-    def getModels(self, brand):
-        try:
-            pathName = f"brands/{brand}"
-            if not path.exists(pathName):
-                return (False, "Бренд не найдены")
             
-            models = []
-            for item in listdir(pathName):
-                full_path = path.join(pathName, item)
-                if path.isfile(full_path):
-                    models.append(item[:-5])
-            return (True, models)
-        except Exception as e:
-            return (False, "Ошибка: " + str(e))
-        
     
     def getDisplays(self, brand, model):
         try:
@@ -137,27 +165,5 @@ class db():
                 ws.cell(row=row, column=3).value = amount
                 wb.save(namePath)
                 return (True, "Успешно отредактирован хеш: " + hash)
-        except Exception as e:
-            return (False, "Ошибка: " + str(e))
-    
-
-    def removeBrand(self, brand):
-        try:
-            namePath = f"brands/{brand}"
-            if not path.exists(namePath):
-                return (False, "Модели не существует.")
-            shutil.rmtree(namePath)
-            return (True, "Успешно удалён бренд: " + brand)
-        except Exception as e:
-            return (False, "Ошибка: " + str(e))
-    
-
-    def removeModel(self, brand, model):
-        try:
-            namePath = f"brands/{brand}/{model}.xlsx"
-            if not path.exists(namePath):
-                return (False, "Модели не существует.")
-            remove(namePath)
-            return (True, "Успешно удаленна модель: " + model)
         except Exception as e:
             return (False, "Ошибка: " + str(e))
